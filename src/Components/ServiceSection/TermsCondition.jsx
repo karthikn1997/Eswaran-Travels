3ca146a6d0c4e02440c9d2fee6764ec9e330366e
Aananthi1234 <anuskpk1208@gmail.com>
2024-10-11 20:46:59 +0530

    final commit

--- src/Components/ServiceSection/TermsCondition.jsx
+++ src/Components/ServiceSection/TermsCondition.jsx
@@ -8,17 +8,11 @@ const TermsCondition = () => {
           Important Information:
         </h2>
         <ul className='pl-6 list-disc tracking-wider text-gray-700 space-y-3'>
-          <li className='text-green-600'>
-            All online transactions with Eswaran Travels are fully secure and your payment details are encrypted.
-          </li>
-          <li>
-            A confirmation message via email and SMS is sent, including the contact details of the driver or tour operator.
-          </li>
           <li>
             Passengers should reach out to the driver or tour operator for any inquiries regarding pickup points or vehicle details.
           </li>
           <li>
-            Please cooperate with the tour operator in case of delays due to traffic or other unforeseen circumstances.
+            Please co-operate with the tour operator in case of delays due to traffic or other unforeseen circumstances.
           </li>
           <li>
             Arrive at the boarding point at least 10 minutes before departure.
@@ -30,7 +24,7 @@ const TermsCondition = () => {
             Pets and animals are strictly not allowed inside the vehicle during the trip.
           </li>
           <li>
-            Smoking and consumption of alcohol are strictly prohibited on the bus.
+            Smoking and consumption of alcohol are strictly prohibited on the vehicle.
           </li>
           <li>
             The management is not liable for any claims, damages, losses, or inconveniences during the trip.
@@ -46,7 +40,7 @@ const TermsCondition = () => {
         <ul className='pl-6 list-disc tracking-wider text-gray-700 space-y-3'>
           <li>Entry tickets at temples or tourist attractions are not included in the package.</li>
           <li>Meals and beverages are excluded from the package.</li>
-          <li>GST is not included in the base fare of the package.</li>
+       
         </ul>
 
         <h2 className='my-5 p-4 text-lg md:text-xl font-semibold tracking-wider text-primary bg-gradient-to-r from-blue-500 to-blue-200 rounded-md'>
