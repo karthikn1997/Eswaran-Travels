b3acc6c03d8bfdfc84453f0dd46a92a5521e8e58
karthikn1997 <karthikn951@gmail.com>
2024-10-12 21:21:08 +0530

    done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from  'react'
+import React, { useEffect } from 'react'
 import Hero from '../Home-Components/Hero'
 import AboutTravel from '../Home-Components/AboutTravel'
 import Packagedetails from '../Home-Components/Packagedetails'
@@ -12,26 +12,31 @@ import { Link } from 'react-router-dom';
 
 
 const Home = () => {
+  // Scroll to the top of the page when the component is mounted
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center overflow-hidden" data-aos="fade-up">
       <Hero />
-      <AboutTravel/>
-      <Packagedetails/>
-      <Banner/>
+      <AboutTravel />
+      <Packagedetails />
+      <Banner />
       <div>
-      <Tabsection/>
-      <div className='flex justify-center items-center'>
-  <Link 
-    to='/services' 
-    className="text-center  sm:w-[60%] md:w-[40%] lg:w-[20%] py-2 px-4 lg:py-3 lg:text-lg text-sm font-medium text-white bg-[#070F4E] rounded-lg tracking-wider hover:bg-secondary transition-all duration-500 active:bg-[#050c3a] flex justify-center items-center gap-3 lg:gap-5 shadow-lg lg:mb-16 mb-5"
-  >
-    Click for More <FaArrowCircleDown className='text-md lg:text-xl' />
-  </Link>
-</div>
+        <Tabsection />
+        <div className='flex justify-center items-center'>
+          <Link
+            to='/services'
+            className="text-center  sm:w-[60%] md:w-[40%] lg:w-[20%] py-2 px-4 lg:py-3 lg:text-lg text-sm font-medium text-white bg-[#070F4E] rounded-lg tracking-wider hover:bg-secondary transition-all duration-500 active:bg-[#050c3a] flex justify-center items-center gap-3 lg:gap-5 shadow-lg lg:mb-16 mb-5"
+          >
+            Click for More <FaArrowCircleDown className='text-md lg:text-xl' />
+          </Link>
+        </div>
 
       </div>
-      <ChooseUs/>
-      <Testimonial/>
+      <ChooseUs />
+      <Testimonial />
     </div>
   )
 }
